6537990e280aaa617f1d613f756623dea5fa9552
Petr Benes <w.benny@outlook.com>
2018-10-02 22:45:14 +0200

    added RS5_17763

---
+++ Win10_1809_RS5_17763/x86/System32/ntoskrnl.exe/Standalone/_VACB_LEVEL_REFERENCE.h
@@ -0,0 +1,6 @@
+typedef struct _VACB_LEVEL_REFERENCE
+{
+  /* 0x0000 */ long Reference;
+  /* 0x0004 */ long SpecialReference;
+} VACB_LEVEL_REFERENCE, *PVACB_LEVEL_REFERENCE; /* size: 0x0008 */
+
---
+++ Win10_1809_RS5_17763/x86/System32/ntoskrnl.exe/Standalone/__WIL__WNF_STATE_NAME.h
@@ -0,0 +1,5 @@
+typedef struct __WIL__WNF_STATE_NAME
+{
+  /* 0x0000 */ unsigned long Data[2];
+} _WIL__WNF_STATE_NAME, *P_WIL__WNF_STATE_NAME; /* size: 0x0008 */
+
