c1681ab336a3e4e61075a15d7aa5e40a7358f24b
Petr Benes <w.benny@outlook.com>
2019-10-18 02:38:10 +0200

    added 1909_19H2

---
+++ Win10_1909_19H2/x64/System32/ntoskrnl.exe/Standalone/_KFLOATING_SAVE.h
@@ -0,0 +1,5 @@
+typedef struct _KFLOATING_SAVE
+{
+  /* 0x0000 */ unsigned long Dummy;
+} KFLOATING_SAVE, *PKFLOATING_SAVE; /* size: 0x0004 */
+
---
+++ Win10_1909_19H2/x64/System32/ntoskrnl.exe/Standalone/_LIST_ENTRY.h
@@ -0,0 +1,6 @@
+typedef struct _LIST_ENTRY
+{
+  /* 0x0000 */ struct _LIST_ENTRY* Flink;
+  /* 0x0008 */ struct _LIST_ENTRY* Blink;
+} LIST_ENTRY, *PLIST_ENTRY; /* size: 0x0010 */
+
